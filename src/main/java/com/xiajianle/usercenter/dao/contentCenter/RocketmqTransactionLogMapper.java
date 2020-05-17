15d9b9f0360121e443ed5cf981d0fdb5983a8ec2
jianlexia <56173042@qq.com>
2020-05-17 10:41:12 +0800

    新增一个rocketMQ的事务日志表

---
+++ src/main/java/com/xiajianle/usercenter/dao/contentCenter/RocketmqTransactionLogMapper.java
@@ -0,0 +1,7 @@
+package com.xiajianle.usercenter.dao.contentCenter;
+
+import com.xiajianle.usercenter.domain.entity.contentCenter.RocketmqTransactionLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface RocketmqTransactionLogMapper extends Mapper<RocketmqTransactionLog> {
+}
